dc15220e6a7123cdd3ada4a0066e6ce395b193c5
nuttakit-vy <56815700+NuttakitDW@users.noreply.github.com>
2021-04-05 11:13:19 +0700

    Update ERC721reciver.sol

--- contracts/ERC721reciver.sol
+++ contracts/ERC721reciver.sol
@@ -1,2 +1,14 @@
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/IERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721Holder.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/IERC721Receiver.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/introspection/ERC165.sol";
 
-
+contract Receiver721Example is IERC721Receiver, ERC165, ERC721Holder {
+    constructor() {
+        _registerInterface(IERC721Receiver.onERC721Received.selector);
+    }
+    
+    function doSomethingWith721Token(IERC721 nftAddress, uint256 tokenId) external {
+        // do something here
+    }
+}
