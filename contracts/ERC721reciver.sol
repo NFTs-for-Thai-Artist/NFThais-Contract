48e08b57277fbc15a561c7b9eda87b3b28cced61
nuttakit-vy <56815700+NuttakitDW@users.noreply.github.com>
2021-04-05 11:15:15 +0700

    Update ERC721reciver.sol

--- contracts/ERC721reciver.sol
+++ contracts/ERC721reciver.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/IERC721.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721Holder.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/IERC721Receiver.sol";
