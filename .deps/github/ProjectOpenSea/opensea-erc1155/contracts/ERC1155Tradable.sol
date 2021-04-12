c98b5b213156872d22bd77c97a7aadcd5314b552
Chomtana <Chomtana001@gmail.com>
2021-04-12 23:54:36 +0700

    Make Opensea ERC1155 work

--- .deps/github/ProjectOpenSea/opensea-erc1155/contracts/ERC1155Tradable.sol
+++ .deps/github/ProjectOpenSea/opensea-erc1155/contracts/ERC1155Tradable.sol
@@ -1,144 +1,10 @@
 pragma solidity ^0.5.12;
 
-import 'https://github.com/ProjectOpenSea/multi-token-standard/contracts/tokens/ERC1155/ERC1155.sol';
-import 'https://github.com/ProjectOpenSea/multi-token-standard/contracts/tokens/ERC1155/ERC1155Metadata.sol';
-import 'https://github.com/ProjectOpenSea/multi-token-standard/contracts/tokens/ERC1155/ERC1155MintBurn.sol';
-
-/*
- * @dev Provides information about the current execution context, including the
- * sender of the transaction and its data. While these are generally available
- * via msg.sender and msg.data, they should not be accessed in such a direct
- * manner, since when dealing with meta-transactions the account sending and
- * paying for execution may not be the actual sender (as far as an application
- * is concerned).
- *
- * This contract is only required for intermediate, library-like contracts.
- */
-contract Context {
-    function _msgSender() internal view returns (address) {
-        return msg.sender;
-    }
-
-    function _msgData() internal view returns (bytes memory) {
-        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
-        return msg.data;
-    }
-}
-
-/**
- * @dev Contract module which provides a basic access control mechanism, where
- * there is an account (an owner) that can be granted exclusive access to
- * specific functions.
- *
- * By default, the owner account will be the one that deploys the contract. This
- * can later be changed with {transferOwnership}.
- *
- * This module is used through inheritance. It will make available the modifier
- * `onlyOwner`, which can be applied to your functions to restrict their use to
- * the owner.
- */
-contract Ownable is Context {
-    address private _owner;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    /**
-     * @dev Initializes the contract setting the deployer as the initial owner.
-     */
-    constructor () public {
-        address msgSender = _msgSender();
-        _owner = msgSender;
-        emit OwnershipTransferred(address(0), msgSender);
-    }
-
-    /**
-     * @dev Returns the address of the current owner.
-     */
-    function owner() public view returns (address) {
-        return _owner;
-    }
-
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        require(owner() == _msgSender(), "Ownable: caller is not the owner");
-        _;
-    }
-
-    /**
-     * @dev Leaves the contract without owner. It will not be possible to call
-     * `onlyOwner` functions anymore. Can only be called by the current owner.
-     *
-     * NOTE: Renouncing ownership will leave the contract without an owner,
-     * thereby removing any functionality that is only available to the owner.
-     */
-    function renounceOwnership() public onlyOwner {
-        emit OwnershipTransferred(_owner, address(0));
-        _owner = address(0);
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
-     */
-    function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        emit OwnershipTransferred(_owner, newOwner);
-        _owner = newOwner;
-    }
-}
-
-library Strings {
-  // via https://github.com/oraclize/ethereum-api/blob/master/oraclizeAPI_0.5.sol
-  function strConcat(string memory _a, string memory _b, string memory _c, string memory _d, string memory _e) internal pure returns (string memory) {
-      bytes memory _ba = bytes(_a);
-      bytes memory _bb = bytes(_b);
-      bytes memory _bc = bytes(_c);
-      bytes memory _bd = bytes(_d);
-      bytes memory _be = bytes(_e);
-      string memory abcde = new string(_ba.length + _bb.length + _bc.length + _bd.length + _be.length);
-      bytes memory babcde = bytes(abcde);
-      uint k = 0;
-      for (uint i = 0; i < _ba.length; i++) babcde[k++] = _ba[i];
-      for (uint i = 0; i < _bb.length; i++) babcde[k++] = _bb[i];
-      for (uint i = 0; i < _bc.length; i++) babcde[k++] = _bc[i];
-      for (uint i = 0; i < _bd.length; i++) babcde[k++] = _bd[i];
-      for (uint i = 0; i < _be.length; i++) babcde[k++] = _be[i];
-      return string(babcde);
-    }
-
-    function strConcat(string memory _a, string memory _b, string memory _c, string memory _d) internal pure returns (string memory) {
-        return strConcat(_a, _b, _c, _d, "");
-    }
-
-    function strConcat(string memory _a, string memory _b, string memory _c) internal pure returns (string memory) {
-        return strConcat(_a, _b, _c, "", "");
-    }
-
-    function strConcat(string memory _a, string memory _b) internal pure returns (string memory) {
-        return strConcat(_a, _b, "", "", "");
-    }
-
-    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
-        if (_i == 0) {
-            return "0";
-        }
-        uint j = _i;
-        uint len;
-        while (j != 0) {
-            len++;
-            j /= 10;
-        }
-        bytes memory bstr = new bytes(len);
-        uint k = len - 1;
-        while (_i != 0) {
-            bstr[k--] = byte(uint8(48 + _i % 10));
-            _i /= 10;
-        }
-        return string(bstr);
-    }
-}
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import 'multi-token-standard/contracts/tokens/ERC1155/ERC1155.sol';
+import 'multi-token-standard/contracts/tokens/ERC1155/ERC1155Metadata.sol';
+import 'multi-token-standard/contracts/tokens/ERC1155/ERC1155MintBurn.sol';
+import "./Strings.sol";
 
 contract OwnableDelegateProxy { }
 
@@ -181,12 +47,12 @@ contract ERC1155Tradable is ERC1155, ERC1155MintBurn, ERC1155Metadata, Ownable {
 
   constructor(
     string memory _name,
-    string memory _symbol//,
-    //address _proxyRegistryAddress
+    string memory _symbol,
+    address _proxyRegistryAddress
   ) public {
     name = _name;
     symbol = _symbol;
-    //proxyRegistryAddress = _proxyRegistryAddress;
+    proxyRegistryAddress = _proxyRegistryAddress;
   }
 
   function uri(
@@ -312,10 +178,10 @@ contract ERC1155Tradable is ERC1155, ERC1155MintBurn, ERC1155Metadata, Ownable {
     address _operator
   ) public view returns (bool isOperator) {
     // Whitelist OpenSea proxy contract for easy trading.
-    /*ProxyRegistry proxyRegistry = ProxyRegistry(proxyRegistryAddress);
+    ProxyRegistry proxyRegistry = ProxyRegistry(proxyRegistryAddress);
     if (address(proxyRegistry.proxies(_owner)) == _operator) {
       return true;
-    }*/
+    }
 
     return ERC1155.isApprovedForAll(_owner, _operator);
   }
@@ -355,8 +221,4 @@ contract ERC1155Tradable is ERC1155, ERC1155MintBurn, ERC1155Metadata, Ownable {
   function _incrementTokenTypeId() private  {
     _currentTokenID++;
   }
-}
-
-contract OpenseaERC1155 is ERC1155Tradable("Opensea ERC1155", "OSERC") {
-
 }
